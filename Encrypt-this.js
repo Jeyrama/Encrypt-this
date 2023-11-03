8e6f22286ad1137a608ea1a2137650d8e89756e3
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-02 18:12:22 -0700

    Update Encrypt-this.js

--- Encrypt-this.js
+++ Encrypt-this.js
@@ -26,3 +26,46 @@ const encryptThis = text => text
   .join(' ');
 
 // or
+
+const encryptWord = w => {
+  const first = w.charCodeAt(0);
+  let res;
+  switch (w.length) {
+    case 0: return '';
+    case 1: return first;
+    case 2: res = [first, w[1]]; break;
+    case 3: res = [first, w[2], w[1]]; break;
+    default: res = [first, w.slice(-1), w.slice(2,-1), w[1]]; break;
+  }
+  return res.join('');
+};
+
+const encryptThisText = text => text.split(' ').map(encryptWord).join(' ');
+
+// or
+
+let encryptThisWord= function(text) {
+  const textArr = text.split(' ');
+  
+  let encrypted = ``; // store the encrypted text
+  
+  for (const word of textArr){
+  	 let len = word.length;
+  	 let crypt;
+  	 // get the ASCII code of the first letter
+  	 let char = word.charCodeAt(0);
+  	 crypt = `${char}`;
+  	 	
+  	 if (len > 2){
+       // works for words' greater than 2
+  	   crypt += `${word[len-1]}${word.slice(2, len-1)}${word[1]}`
+  	 } else if (len == 2){
+  	 	 crypt += `${word.slice(1)}`
+  	 } else{
+       // assumes it is a single letter
+     }
+  	 encrypted += crypt + ' ';
+  }
+  encrypted = encrypted.trim(); // removes trailing spaces
+  return encrypted;
+}
