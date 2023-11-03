502c0a330dceffada71d0c0a80ac6ab8acd9fca7
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-02 18:10:13 -0700

    Update Encrypt-this.js

--- Encrypt-this.js
+++ Encrypt-this.js
@@ -17,3 +17,10 @@ Examples:
 
 
 // Solution
+
+const encryptThis = text => text
+  .split(' ')
+  .map(word => word
+  .replace(/(^\w)(\w)(\w*)(\w$)/, `$1$4$3$2`)
+  .replace(/^\w/, word.charCodeAt(0)))
+  .join(' ');
